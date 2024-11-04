68ec0499edf6983f7c6948c75ec858ab6551249a
Pablo Gutiérrez <pa.gutierrezu@duocuc.cl>
2024-11-03 22:16:33 -0300

    Repaso de API con Aves de CHile Api

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { AvesApiService } from '../services/aves-api.service';
+import { Ave } from '../interfaces/ave';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  aves: Ave[] = [];
+  constructor(private readonly service: AvesApiService) {};
+
+  ngOnInit(): void{
+
+    const listaAves = localStorage.getItem('aves');
+
+    if(listaAves){
+      this.aves = JSON.parse(listaAves);
+      console.log('Aves desde Local Storage')
+    } else {
+      this.service.obtenerAves().subscribe((data: Ave[])=>{
+        localStorage.setItem('aves', JSON.stringify(data));
+        this.aves = data;
+        console.log('Aves desde Api')
+      });
+    }
+  };
+
+
+}
